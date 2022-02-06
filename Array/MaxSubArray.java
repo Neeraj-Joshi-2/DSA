f75d2f1a2a8afcc44fafe40cdcf16039c01f8918
Neeraj Joshi <79934749+Neeraj-Joshi-2@users.noreply.github.com>
2022-02-06 10:05:42 +0530

    Add files via upload

---
+++ Array/MaxSubArray.java
@@ -0,0 +1,21 @@
+public class MaxSubArray{
+
+    static int largestSumContiguousArray(int arr[]){
+        int max = Integer.MIN_VALUE;
+        int sum = 0 ;
+        for (int i = 0; i < arr.length; i++) {
+            sum = sum + arr[i] ;
+            max = Math.max(max , sum) ;
+            if(sum < 0){
+                sum = 0 ;
+            }   
+        }
+        return max ;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {-2, -3, 4, -1, -2, 1, 5, -3} ;
+        System.out.println(largestSumContiguousArray(arr));
+        
+    }
+}
