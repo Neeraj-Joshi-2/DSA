d2697e142fca537d46b880e2d99d064381fc5625
Neeraj Joshi <79934749+Neeraj-Joshi-2@users.noreply.github.com>
2022-02-07 23:50:12 +0530

    Update CyclicallyRotate.java

--- Array/CyclicallyRotate.java
+++ Array/CyclicallyRotate.java
@@ -5,7 +5,7 @@ public class CyclicallyRotate {
  
     static void rotate() {
         int x = arr[arr.length - 1], i;
-        for (i = arr.length - 1; i > 0; i--) 
+        for (i = arr.length - 1; i > 0; i--)  
             arr[i] = arr[i - 1];
         arr[0] = x;
     }
